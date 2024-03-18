730e7a6625f49c4d11a6d5672ad5b0352591bfd9
hariyanijenish <hariyanijenish@gmail.com>
2024-03-18 18:07:13 +0530

    data modephay

--- lib/home page.dart
+++ lib/home page.dart
@@ -1,7 +1,6 @@
-
 import 'package:pr_4/Laptop.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 
 
@@ -11,17 +10,18 @@ class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0xffDFF5CE),
       appBar: AppBar(
-        
-        title: const Text("Home Page ",
+        backgroundColor: Color(0xffDEE2FC),
+        title: const Text(" Home Page ",
         style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-          color: Colors.green,
+          color: Color(0xff525B88),
+             fontWeight: FontWeight.w700,
+             fontSize: 30,
         ),
-        ),
+      ),
         centerTitle: true,
+        
         actions: const [
           Icon(
             Icons.shopping_cart,
@@ -30,76 +30,100 @@ class homepage extends StatelessWidget {
             width: 15,
           ),
         ],
-        backgroundColor: const Color(0xffDFE3FC),
       ),
-      body: SingleChildScrollView(
+      body: 
+      SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        child: Row(
+      child: Row(
           children: [
-            ...allproducts
-            .map(
-              (e) =>  Container(
-                height: 200,
-                width: 200,
-          child : 
-              Text("Buisness Type"),
-           Row(
+        ...allproducts.map(
+          (e) => Container(
+            margin: const EdgeInsets.all(10),
+            height: 330,
+            width: 200,
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 5),
+                  blurRadius: 5,
+                  spreadRadius: -2,
+                  color: Colors.grey,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Column(
               children: [
-                 Align(
-                  alignment: Alignment.center,
-               child: CircleAvatar(
-                  radius: 90,
-                  backgroundColor: Colors.black,
-                  child: CircleAvatar(
-                    radius: 85,
-                    backgroundImage: NetworkImage("${e['thumbnail']}"),
-                  ),
-                ),
-                ),
                 Expanded(flex: 2,
                  child: Container(
-                  decoration: const BoxDecoration(
+                  decoration:  BoxDecoration(
+                    color: Color(0xff87B049),
                     borderRadius: BorderRadius.only(
                       topRight: Radius.circular(20),
                       topLeft: Radius.circular(20),
-                      
                     ),   
-                  ),
-                 ),),
+                    image: DecorationImage(
+                    image: NetworkImage(e['thumbnail']),
+                    fit: BoxFit.cover, 
+                    )
+                   ),
+                 )),
                 Expanded(
                     flex: 1,
                     child: Container(
+                     
                       padding: const EdgeInsets.all(5),
                       decoration: const BoxDecoration(
-                          color: Colors.white,
+                           color: Color(0xffDEE2FC),
                           borderRadius: BorderRadius.only(
                                                                 bottomLeft: Radius.circular(20),
                             bottomRight: Radius.circular(20),
                           )),
-                      child: const Align(
-                        alignment: Alignment.center,
+                      child: Align(
+                        alignment: Alignment.topLeft,
                         child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             Text(
-                              "Take The better for You ",
-                              style: TextStyle(
+                              "${e['title']}${e['display']}${e['core']}${e['type']}${e['brand']}${e['ram']}${e['ssd']}",
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
                                 fontSize: 18,
                                 fontWeight: FontWeight.w900,
                               ),
                             ),
-                           
+                            Text(
+                              "${e['price']}",
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            RatingBarIndicator(
+                              rating: e['rating'].toDouble(),
+
+                                itemCount: 5,
+                                itemSize: 25,
+                                itemBuilder: (context, i) {
+                                  return const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                  );
+                                }),
+                              ],
+                            ),
+                          ),
+                        )),
                       ],
                     ),
                   ),
-                )),
-              ],
+                ),
+              ].toList()
             ),
-          ),)
-          ],
-        ),
-      )
-       
+          ),
         );
-      }
+       }
 }
